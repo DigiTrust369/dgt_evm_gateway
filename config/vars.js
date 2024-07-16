91d7563055d5a7a8ef7ab161c13afa2f191dd092
Nguyen Nhat <nhatlapross@gmail.com>
2024-07-16 22:21:14 +0700

    update

--- config/vars.js
+++ config/vars.js
@@ -26,7 +26,7 @@ module.exports = Object.freeze({
       contractOwnerAddr: process.env.DGT_CONTRACT_OWNER_ADDR || '0x68E8F29BDD06A2Be2E387C96AD5cdB0B34D101B5',
       providerUrl: process.env.DGT_API_URL || 'https://api.baobab.klaytn.net:8651',
       dgtTokenAddress: process.env.DGT_TOKEN_ADDRESS || '0xee42Cf6E3E575b5aBC2B3Ae760BA1AF2c05791df',
-      dgtWalletAddress: process.env.DGT_WALLET_ADDRESS || '0x0D0Df554db5623Ba9A905D0bE4C6bAc48144841E',
+      dgtWalletAddress: process.env.DGT_WALLET_ADDRESS || '0x68E8F29BDD06A2Be2E387C96AD5cdB0B34D101B5',
       dgtVaultSignalAddress: process.env.DGT_VAULT_SIGNAL_ADDRESS || '0xA39C2b7957496d28Cd6B3fD005b4A1584Abe0c30',
       dgtAdminAddress: process.env.DGT_ADMIN_ADDRESS || '0xF7FCCFc3DE0789362B5B998782992a27b12040c8',
       dgtAdminPriv: process.env.DGT_ADMIN_PRIV || '6ee44874d355c054c138a417c5a725cccf7353460892125e028e60ebc8c77129',
@@ -35,7 +35,7 @@ module.exports = Object.freeze({
   },
   dgtPriceURL: process.env.dgt_PRICE_URL || "https://dgt-dbank-monitor-api.dgt-dbank-monitor-dev.vncdevs.com/hook/mt5_pricings/price?symbol=",
   contractParams:{
-    from    : process.env.dgt_CONTRACT_OWNER_ADDR || '0x0D0Df554db5623Ba9A905D0bE4C6bAc48144841E',
+    from    : process.env.dgt_CONTRACT_OWNER_ADDR || '0x68E8F29BDD06A2Be2E387C96AD5cdB0B34D101B5',
     gasPrice: 25000000000,
     gasLimit: 8500000,
   },
